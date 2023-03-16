9a4013baec19b764a756d4b254a39fd440bf9ec4
Adel Sliman <adelasliman@gmail.com>
2023-03-15 23:35:44 -0400

    upate url

--- server/server.js
+++ server/server.js
@@ -47,4 +47,4 @@ app.post('/', async (req, res) =>{
    } 
 })
 
-app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
+app.listen(5000, () => console.log('Server is running on port https://codex-t9da.onrender.com'));
